8aa8388eaebb9f84348623b06f61e22b554186d5
JGokhale <101219999+JG-Perficient@users.noreply.github.com>
2025-04-17 13:16:07 -0400

    JG :: update the fetch query

--- data/web-resources/js/graphQLMutations/fragmentMutations.js
+++ data/web-resources/js/graphQLMutations/fragmentMutations.js
@@ -1,13 +1,18 @@
 import { utilities } from "./utility.js";
 
 const fetchPromoContentFromCF = async () => {
-    const query = JSON.stringify({
-      query:  ""
-    });
+    /* const query = JSON.stringify({
+      query:  `query GetBannerByPath($path: String!) { shoppingCartBannerModelByPath(_path: $path){ item { bannerTitle bannerDescription { plaintext } imagePath { ... on DocumentRef { _path _publishUrl title } } ctaText ctaUrl } } }`,
+      variables: {
+        "path": "/content/dam/LumaX/shopping-cart/test-banner"
+        },
+    }); */
+    // const fetchurl = utilities.GRAPHQL_ENDPOINT;
+    const cfEndPoint = `/content/dam/LumaX/shopping-cart/test-banner`;
+    const fetchurl = `/graphql/execute.json/lumax/BannerByPath;path=${cfEndPoint}`;
+    const promotionContent = await utilities.fetchRequests(fetchurl, 'GET', utilities.HEADERS);
   
-    const promotionContent = await utilities.fetchRequests(utilities.GRAPHQL_ENDPOINT, 'GET', utilities.HEADERS, query);
-  
-    return promotionContent.data;
+    return promotionContent.data.shoppingCartBannerModelByPath.item;
   }
 
   export const fragmentMutations = {
