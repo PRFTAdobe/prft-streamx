615257f4a6352836f13a5b5c7a93527295c22684
prakashprft <22453786+prakashprft@users.noreply.github.com>
2025-04-18 10:40:51 -0700

    adding cache buster
    Signed-off-by: prakashprft <22453786+prakashprft@users.noreply.github.com>

--- data/web-resources/js/graphQLMutations/aemFragmentsMutations.js
+++ data/web-resources/js/graphQLMutations/aemFragmentsMutations.js
@@ -1,7 +1,7 @@
 import { utilities } from "./utility.js";
 
 const fetchPromoContentFromCF = async () => {
-    const fetchurl = `${utilities.AEM_GRAPHQL_ENDPOINT}/execute.json/lumax/BannerByPath;path=${utilities.CF_PROMO_PATH}`;
+    const fetchurl = `${utilities.AEM_GRAPHQL_ENDPOINT}/execute.json/lumax/BannerByPath;path=${utilities.CF_PROMO_PATH}?cache=no`;
     const promotionContent = await utilities.fetchRequests(fetchurl, 'GET', utilities.HEADERS);
   
     return promotionContent.data.shoppingCartBannerModelByPath.item;
