01fe265b0a0027b94b90308ab44f6e7d6f7c6675
prakashprft <22453786+prakashprft@users.noreply.github.com>
2025-04-21 15:38:28 -0700

    adding timestamp to url
    Signed-off-by: prakashprft <22453786+prakashprft@users.noreply.github.com>

--- data/web-resources/js/graphQLMutations/aemFragmentsMutations.js
+++ data/web-resources/js/graphQLMutations/aemFragmentsMutations.js
@@ -1,7 +1,9 @@
 import { utilities } from "./utility.js";
 
 const fetchPromoContentFromCF = async () => {
-    const fetchurl = `${utilities.AEM_GRAPHQL_ENDPOINT}/execute.json/lumax/BannerByPath;path=${utilities.CF_PROMO_PATH}`;
+    const now = new Date();
+    const timestamp = now.getTime(); 
+    const fetchurl = `${utilities.AEM_GRAPHQL_ENDPOINT}/execute.json/lumax/BannerByPath;path=${utilities.CF_PROMO_PATH}?ts=${timestamp}`;
     const promotionContent = await utilities.fetchRequests(fetchurl, 'GET', utilities.HEADERS);
   
     return promotionContent.data.shoppingCartBannerModelByPath.item;
