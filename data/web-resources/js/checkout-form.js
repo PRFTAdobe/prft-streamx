81832d37aa557a2dc7fc6724bb553f94ac64e492
Camilo Torres A <c.torresalzate@gmail.com.com  git config --global user.name CCTorres>
2026-01-19 16:24:04 -0500

    CT::Adding purchase event for B2B place order

--- data/web-resources/js/checkout-form.js
+++ data/web-resources/js/checkout-form.js
@@ -112,7 +112,6 @@ const updateStep2State = async () => {
         }
         console.log(response.errors);
     }
-    sessionStorage.setItem("orderProducts",JSON.stringify(response.items))
     return isError;
 };
 
@@ -239,9 +238,7 @@ const showStep3 = async () => {
             if (!isError) {
                 document.querySelector('.order-number').innerHTML = placeOrderNumber;
                 var totalCost = document.querySelector(".total-payment").innerHTML;
-                const orderProducts = JSON.parse(sessionStorage.getItem("orderProducts"));
-                purchaseOrderEvent(placeOrderNumber, totalCost, orderProducts);
-                sessionStorage.removeItem("orderProducts");
+                purchaseOrderEvent(placeOrderNumber, totalCost);
                 userSession.clearCart();
                 updateCartCountOnUI();
 
