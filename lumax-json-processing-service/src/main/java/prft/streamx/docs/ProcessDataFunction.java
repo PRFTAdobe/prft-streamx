f25e51f4b49d11cb7a559dbd8e1dd3c4e70aaac5
Hemanth Sreenu Neelam <hemanth.sreenuneelam@perficient.com>
2026-02-15 16:07:44 +0530

    updated processing service

--- lumax-json-processing-service/src/main/java/prft/streamx/docs/ProcessDataFunction.java
+++ lumax-json-processing-service/src/main/java/prft/streamx/docs/ProcessDataFunction.java
@@ -1,36 +1,23 @@
 package prft.streamx.docs;
 
-import static dev.streamx.quasar.reactive.messaging.utils.MetadataUtils.extractAction;
-import static dev.streamx.quasar.reactive.messaging.utils.MetadataUtils.extractEventTime;
-import static dev.streamx.quasar.reactive.messaging.utils.MetadataUtils.extractKey;
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import dev.streamx.quasar.reactive.messaging.metadata.Action;
-import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.GenericPayload;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Message;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
-import io.smallrye.mutiny.Multi;
+import dev.streamx.blueprints.data.Data;
 import org.jboss.logging.Logger;
-import prft.streamx.docs.configuration.Configuration;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
 
 @ApplicationScoped
-public class ProcessDataFunction extends AbstractFunction {
+public class ProcessDataFunction {
 
     private static final String CHANNEL_AGGREGATED_DATA = "aggregated-data";
     private static final String CHANNEL_CUSTOM_AGGREGATED_DATA = "custom-aggregated-data";
-
-    protected final Map<Configuration, Set<String>> supportedNamespacesByConfig
-            = new LinkedHashMap<>();
 
     @Inject
     Logger log;
@@ -38,42 +25,11 @@ public class ProcessDataFunction extends AbstractFunction {
 
     @Incoming(CHANNEL_AGGREGATED_DATA)
     @Outgoing(CHANNEL_CUSTOM_AGGREGATED_DATA)
-    Multi<Message<Data>> process(Message<Data> message) {
-        String key = extractKey(message);
-        log.info("Inside ProcessDataFunction");
-        Action action = extractAction(message);
-        Long eventTime = extractEventTime(message);
-        try {
-            log.info("Processing message :: " + key + " :: Action :: " + action);
-
-            String[] parts = key.split(KEY_SEPARATOR);
-            if (parts.length < 2) {
-                log.warnf("Invalid key format, skipping: %s", key);
-                return Multi.createFrom().empty();
-            }
-            final String id = parts[ID_POSITION];
-            final String namespace = parts[NAMESPACE_POSITION];
-
-            if (!namespace.equalsIgnoreCase("product")) {
-                log.tracef("Not a product, skipping: %s", key);
-                return Multi.createFrom().empty();
-            }
-
-            // Forward UNPUBLISH as UNPUBLISH (don’t emit a "publish" with empty content)
-            if (Action.UNPUBLISH.equals(action)) {
-                log.info("Processing Unpublish event");
-                Message<Data> out = createUnpublishMessage(id, eventTime, namespace);
-                return Multi.createFrom().item(out)
-                        .onCompletion().call(() -> Uni.createFrom().completionStage(message.ack()));
-            }
-
-            final Data payload = message.getPayload();
-            String json = payload.getContentAsString();
-
-            log.info("JSON data of product :: " + json);
-
-            JsonObject gsonObj = JsonParser.parseString(json).getAsJsonObject();
-
+    public GenericPayload<Data> processRequest(Data data, Action action) {
+        if (Action.PUBLISH.equals(action)) {
+            String dataString = new String(data.getContent().array());
+            log.infof("In: %s", dataString);
+            JsonObject gsonObj = JsonParser.parseString(dataString).getAsJsonObject();
 
             JsonElement slugEl = gsonObj.get("slug");
             String slug = (slugEl != null && !slugEl.isJsonNull()) ? slugEl.getAsString() : "";
@@ -94,15 +50,14 @@ public class ProcessDataFunction extends AbstractFunction {
             view2Gson.addProperty("url", "/products/view2/"+slug+".html");
             gsonArray.add(view2Gson);
             gsonObj.add("views", gsonArray);
-            log.info("GsonObject :: "+gsonObj);
-        }
-        catch (Exception e){
-            message.nack(e);
-            return Multi.createFrom().empty();
-        }
 
-
-        return Multi.createFrom().empty();
+            Data updatedData = new Data(gsonObj.toString());
+            log.infof("Out: %s", new String(updatedData.getContent().array()));
+            return GenericPayload.of(updatedData);
+        } else if (Action.UNPUBLISH.equals(action)) {
+            return GenericPayload.of(null);
+        } else {
+            return null;
+        }
     }
-
 }
