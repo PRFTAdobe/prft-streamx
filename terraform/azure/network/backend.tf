0f9d3a9fcfba06372793655929be06e073a3068b
Prakash Venkatesh <prakash.venkatesh@perficient.com>
2026-02-24 06:40:36 -0800

    fixing the backend state name

--- terraform/azure/network/backend.tf
+++ terraform/azure/network/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
       resource_group_name  = "rg-adobe-summit-demo"
-      storage_account_name = "tfstatep8uvb"
+      storage_account_name = "tfstatedihsg"
       container_name       = "streamx-storagecontainer"
       key                  = "network.tfstate"
   }
