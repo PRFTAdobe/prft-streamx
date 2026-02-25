ad3f6d42f994579f9d81400ac32b08c8edb229c4
Prakash Venkatesh <prakash.venkatesh@perficient.com>
2026-02-24 16:39:23 -0800

    Fixed the backend storage account infos

--- terraform/azure/monitoring-storage/backend.tf
+++ terraform/azure/monitoring-storage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-adobe-summit-demo"
-    storage_account_name = "tfstatedihsg"
+    storage_account_name = "tfstateo7n14"
     container_name       = "streamx-tfstate"
     key                  = "monitoring_storage.tfstate"
   }
