a3b92f5eee0f30125aa2d169115b7cd299de9f0e
Marek Czajkowski <marek.czajkowski@ds.pl>
2026-02-26 14:15:57 +0100

    Rolling back container_name of backend state for monitoring module

--- terraform/azure/monitoring-storage/backend.tf
+++ terraform/azure/monitoring-storage/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-adobe-summit-demo"
     storage_account_name = "tfstateo7n14"
-    container_name       = "streamx-commerce-accelerator-monitoring"
+    container_name       = "streamx-tfstate"
     key                  = "monitoring_storage.tfstate"
   }
 }
